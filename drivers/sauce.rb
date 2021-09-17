329c09339dfca147fdbed12652d2823cfa2994d2
Alexey Voronenko <alexey.voronenko96@gmail.com>
2021-09-17 18:08:54 +0300

    Update sauce.rb

--- drivers/sauce.rb
+++ drivers/sauce.rb
@@ -5,7 +5,9 @@ Capybara.register_driver :sauce do |app|
     maxDuration: Howitzer.cloud_max_duration,
     idleTimeout: Howitzer.cloud_sauce_idle_timeout,
     recordScreenshots: Howitzer.cloud_sauce_record_screenshots,
-    videoUploadOnPass: Howitzer.cloud_sauce_video_upload_on_pass
+    videoUploadOnPass: Howitzer.cloud_sauce_video_upload_on_pass,
+    username: Howitzer.cloud_auth_login,
+    access_key: Howitzer.cloud_auth_pass
   )
   if Howitzer.user_agent.present?
     if CapybaraHelpers.chrome_browser?
@@ -16,7 +18,7 @@ Capybara.register_driver :sauce do |app|
       caps[:firefox_profile] = profile
     end
   end
-  url = "https://#{Howitzer.cloud_auth_login}:#{Howitzer.cloud_auth_pass}@ondemand.saucelabs.com/wd/hub"
+  url = 'https://ondemand.us-west-1.saucelabs.com/wd/hub'
   CapybaraHelpers.cloud_driver(app, caps, url)
 end
 
