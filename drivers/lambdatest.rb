587271a1df0fcf86984c08d461676a9aabd8a735
Tymofii Mikriukov <tmikriukov@strongsd.com>
2022-07-08 12:48:28 +0300

    Selenium W3C compatibility

--- drivers/lambdatest.rb
+++ drivers/lambdatest.rb
@@ -1,14 +1,21 @@
 Capybara.register_driver :lambdatest do |app|
-  caps = CapybaraHelpers.required_cloud_caps.merge(
-    project: Howitzer.cloud_lambdatest_project,
-    build: Howitzer.cloud_lambdatest_build,
-    nativeEvents: true,
-    'ie.ensureCleanSession': 'true',
-    acceptSslCerts: true
-  )
-  caps[:resolution] = Howitzer.cloud_lambdatest_resolution if Howitzer.cloud_lambdatest_resolution.present?
-  caps[:device] = Howitzer.cloud_lambdatest_mobile_device if Howitzer.cloud_lambdatest_mobile_device.present?
   url = "https://#{Howitzer.cloud_auth_login}:#{Howitzer.cloud_auth_pass}@hub.lambdatest.com/wd/hub"
+  if Gem::Requirement.new('>=4').satisfied_by?(Gem::Version.new(Selenium::WebDriver::VERSION))
+    caps = CapybaraHelpers.required_w3c_cloud_caps
+    lt_options = {
+      name: "#{(Howitzer.current_rake_task || 'ALL').upcase} #{Howitzer.cloud_browser_name}",
+      build: Howitzer.cloud_lambdatest_build
+    }
+    lt_options[:platformName] = Howitzer.cloud_platform unless Howitzer.cloud_platform.casecmp?('any')
+    lt_options[:resolution] = Howitzer.cloud_lambdatest_resolution if Howitzer.cloud_lambdatest_resolution.present?
+    caps['LT:Options'] = lt_options
+  else
+    caps = CapybaraHelpers.required_cloud_caps.merge(
+      build: Howitzer.cloud_lambdatest_build,
+      acceptSslCerts: true
+    )
+    caps[:resolution] = Howitzer.cloud_lambdatest_resolution if Howitzer.cloud_lambdatest_resolution.present?
+  end
   CapybaraHelpers.cloud_driver(app, caps, url)
 end
 
